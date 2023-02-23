93961a5b6f7cf7d30a3c71b4a68e93a6bdd87699
jjhiggz <jonathan.higger@gmail.com>
2023-02-23 09:30:30 -0700

    updated the riddles to be more clear about the vocab words

--- problems/p10.ts
+++ problems/p10.ts
@@ -1,5 +1,4 @@
 import { prisma } from "./prisma";
 
 // Deleting a thing, only works swell, if things that reference it are deleted as well
-// this is because a favorite references the user via a foreign key
 export const deleteAllUsersWithAgeUnderN = async (n: number) => {};
--- problems/p3.ts
+++ problems/p3.ts
@@ -1,5 +1,5 @@
 import { prisma } from "./prisma";
 
 // Get the average age of all users
-// hint: the hot tub is hot, the water is great, to solve this problem you should aggregate
+// hint: the hot tub is hot, the water is great, to solve this problem you should "aggregate"
 export const getAverageUserAge = async () => {};
