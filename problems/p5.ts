8db6777b34991de70fab78cc51cc116334cf0acd
Peter Castillo <pcastillo@genuinehealthgroup.com>
2024-01-27 11:27:49 -0500

    Updated return type for p5 and p6

--- problems/p5.ts
+++ problems/p5.ts
@@ -1,11 +1,11 @@
 import { groupBy, map, reduce, sumBy } from "remeda";
 import { prisma } from "./prisma";
-import { StarRating } from "@prisma/client";
+import { Movie } from "@prisma/client";
 
 // hint:find all stars with the movies "included" on, then good ol' javascript should finish the job
 // This one should require more javascript work than the previous ones
 export const getAllMoviesWithAverageScoreOverN = async (n: number) => {
-  return prisma.$queryRaw`
+  return prisma.$queryRaw<Movie[]>`
         select *
         from 
             movie
--- problems/p6.ts
+++ problems/p6.ts
@@ -1,8 +1,9 @@
 import { prisma } from "./prisma";
+import { Movie } from "@prisma/client";
 
 // find all movies that a user has watched
 export const findAllMoviesThatAUserWatched = async (userId: number) => {
-  return prisma.$queryRaw`
+  return prisma.$queryRaw<Movie[]>`
         select *
         from 
             movie
